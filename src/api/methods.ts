e275f56726e47024e6b2f79e0a6e8e29768a5c88
morgota199 <morgota199@gmail.com>
2023-09-12 13:06:02 +0300

    feat(update): polling

--- src/api/methods.ts
+++ src/api/methods.ts
@@ -28,9 +28,7 @@ import { WrapRequest } from "../types/wrap-request";
 import { Client } from "./client";
 
 export class Methods {
-	constructor(private readonly token: string) {
-		console.log(token);
-	}
+	constructor(private readonly token: string) {}
 
 	private async _send<T>(
 		methodName: string,
